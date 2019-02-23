000e232dee0eb99136d20d060edbe422c091fcb7
Mathew Bramson <mathewbramson@gmail.com>
2019-02-23 15:30:58 -0500

    Initial Commit

---
+++ lib/two_factor_in_a_can.ex
@@ -0,0 +1,19 @@
+defmodule TwoFactorInACan do
+  @moduledoc """
+  `TwoFactorInACan` provides easy to use functions for implementing two factor
+  authentication in a phoenix application.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TwoFactorInACan.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/two_factor_in_a_can_test.exs
@@ -0,0 +1,8 @@
+defmodule TwoFactorInACanTest do
+  use ExUnit.Case
+  doctest TwoFactorInACan
+
+  test "greets the world" do
+    assert TwoFactorInACan.hello() == :world
+  end
+end
