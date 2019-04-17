17288d48c4b8509c04450bc791f39a8e7c30f8e1
Mathew Bramson <mathewbramson@gmail.com>
2019-04-17 19:06:46 -0400

    Configure app for hex

--- mix.exs
+++ mix.exs
@@ -5,8 +5,11 @@ defmodule TwoFactorInACan.MixProject do
     [
       app: :two_factor_in_a_can,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps(),
       name: "TwoFactorInACan",
       source_url: "https://github.com/mbramson/two_factor_in_a_can",
@@ -24,6 +27,22 @@ defmodule TwoFactorInACan.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description() do
+    """
+    TwoFactorInACan provides easy to use functions for implementing two factor
+    authentication.
+    """
+  end
+
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Mathew Bramson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mbramson/two_factor_in_a_can"}
     ]
   end
 
