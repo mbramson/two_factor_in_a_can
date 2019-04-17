21055815be373826862a90b1e7a0430e837598d0
Mathew Bramson <mathewbramson@gmail.com>
2019-04-17 19:04:21 -0400

    Remove TwoFactorInACan.Hello

--- lib/two_factor_in_a_can.ex
+++ lib/two_factor_in_a_can.ex
@@ -3,17 +3,4 @@ defmodule TwoFactorInACan do
   `TwoFactorInACan` provides easy to use functions for implementing two factor
   authentication in a phoenix application.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TwoFactorInACan.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
