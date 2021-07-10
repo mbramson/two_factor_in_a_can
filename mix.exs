8c027846e3a2cac1d3efc519cb786d1a12379582
Mathew Bramson <mathewbramson@gmail.com>
2021-07-10 16:33:05 -0400

    Bump version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TwoFactorInACan.MixProject do
   def project do
     [
       app: :two_factor_in_a_can,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
